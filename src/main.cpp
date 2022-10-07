591db028fe893b1fd3f6b631d03ba811a986e03f
Kevin Clement <kevin.clement@gmail.com>
2022-10-06 19:20:23 -0700

    move pi check to function

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,10 @@
 #define LED_2_DATA A5
 
 // LED Settings
-#define NUM_LEDS 70
+#define NUM_LEDS            70
 #define BRIGHTNESS          96
-#define FRAMES_PER_SECOND  120
-#define NUM_TRIANGLE_LEDS 12
+#define FRAMES_PER_SECOND   120
+#define NUM_TRIANGLE_LEDS   12
 int triangle[] = {25, 29, 30, 31, 32, 33, 38, 37, 21, 22, 23, 24};
 
 // Debug flag
@@ -34,7 +34,8 @@ CRGB leds_2[NUM_LEDS];
 // Controls whether to drive LEDs or use external control
 bool inControlOfLEDs = true;
 
-Bounce2::Button button = Bounce2::Button();
+Bounce bounce = Bounce();
+
 int current_debug_led = 0;
 uint8_t gHue = 0;
 
@@ -52,9 +53,9 @@ void setup() {
     pinMode(LED_1_MUX, OUTPUT);
     pinMode(LED_2_MUX, OUTPUT);
 
-    button.attach(PIN_PI_CONTROL, INPUT);
-    button.interval(5); 
-    button.setPressedState(LOW); 
+    // Uses an external pull down
+    bounce.attach( PIN_PI_CONTROL, INPUT ); 
+    bounce.interval(5); 
 }
 
 void animateCircle() {
@@ -87,28 +88,27 @@ void debug() {
     }
 }
 
-void loop() {
-    debug();
-    
+void checkForPiSignal() {
+    bounce.update();
 
-    button.update();
+    // TODO: Should we implement an ultimate time-out?  
+    //  Say 30s and we just hand it over
 
-    if ( button.pressed() ) {
-        if (!inControlOfLEDs) {
-            Serial.println("Taking over control of LEDs...");
-            inControlOfLEDs = true;
-        }
-        else {
-            Serial.println("Giving control of LEDs to FAST...");
-            // leds[0] = CRGB::Red;
-            // leds[1] = CRGB::Red;
-            // leds[2] = CRGB::Red;
-            inControlOfLEDs = false;
-        }
+    if (bounce.rose()) {
+        Serial.println("Giving control of LEDs to FAST...");
+        inControlOfLEDs = false;
     }
 
+    // Write out the control for the Mux
     digitalWrite(LED_1_MUX, inControlOfLEDs ? HIGH : LOW);
     digitalWrite(LED_2_MUX, inControlOfLEDs ? HIGH : LOW);
+}
+
+void loop() {
+    debug();
+
+    // check for the Pi signaling us that it booted
+    checkForPiSignal();
 
     animateCircle();
 
