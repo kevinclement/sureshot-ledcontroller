f5931cd8d0b218944cfa7e33b9b24fdec93dfff4
Kevin Clement <kevin.clement@gmail.com>
2022-10-06 18:43:32 -0700

    basic animation proof

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@
 #define LED_2_MUX  6
 #define LED_2_DATA A5
 
-#define NUM_LEDS 3
+#define NUM_LEDS 70
+
+#define BRIGHTNESS          96
+#define FRAMES_PER_SECOND  120
 
 // Define the array of leds
-CRGB leds[NUM_LEDS];
+// CRGB leds[NUM_LEDS];
 CRGB leds_2[NUM_LEDS];
 
 // Controls whether to drive LEDs or use external control
@@ -31,34 +34,44 @@ bool inControlOfLEDs = true;
 Bounce2::Button button = Bounce2::Button();
 
 void setup() {
-  Serial.begin(9600);
-  Serial.print("SureShot LED Boot by kevinc...\n");
+    Serial.begin(9600);
+    Serial.setTimeout(10);
+    Serial.print("SureShot LED Boot by kevinc...\n");
 
-  FastLED.addLeds<WS2812, LED_1_DATA, RGB>(leds, NUM_LEDS);
-  FastLED.addLeds<WS2812, LED_2_DATA, RGB>(leds_2, NUM_LEDS);
+    // FastLED.addLeds<WS2812, LED_1_DATA, RGB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_2_DATA, GRB>(leds_2, NUM_LEDS)
+           .setCorrection(TypicalPixelString);
+    FastLED.setBrightness(BRIGHTNESS);
 
-  pinMode(PIN_PI_CONTROL, INPUT);
-  pinMode(LED_1_MUX, OUTPUT);
-  pinMode(LED_2_MUX, OUTPUT);
+    pinMode(PIN_PI_CONTROL, INPUT);
+    pinMode(LED_1_MUX, OUTPUT);
+    pinMode(LED_2_MUX, OUTPUT);
 
-  button.attach(PIN_PI_CONTROL, INPUT);
-  button.interval(5); 
-  button.setPressedState(LOW); 
+    button.attach(PIN_PI_CONTROL, INPUT);
+    button.interval(5); 
+    button.setPressedState(LOW); 
 }
+
+int currentLed = 6;
+
+#define NUM_TRI_LEDS 12
+int triOrder[] = {25, 29, 30, 31, 32, 33, 38, 37, 21, 22, 23, 24};
+
+bool filledIt = false;
+int currentTriIndex = 0;
+uint8_t gHue = 0;
+int pos = -1;
 
 void loop() {
     button.update();
 
     if ( button.pressed() ) {
         if (!inControlOfLEDs) {
-            Serial.print("Taking over control of LEDs...");
-            // leds[0] = CRGB::Blue;
-            // leds[1] = CRGB::Blue;
-            // leds[2] = CRGB::Blue;
+            Serial.println("Taking over control of LEDs...");
             inControlOfLEDs = true;
         }
         else {
-            Serial.print("Giving control of LEDs to FAST...");
+            Serial.println("Giving control of LEDs to FAST...");
             // leds[0] = CRGB::Red;
             // leds[1] = CRGB::Red;
             // leds[2] = CRGB::Red;
@@ -69,13 +82,53 @@ void loop() {
     digitalWrite(LED_1_MUX, inControlOfLEDs ? HIGH : LOW);
     digitalWrite(LED_2_MUX, inControlOfLEDs ? HIGH : LOW);
 
-    leds[0] = CRGB::Blue;
-    leds[1] = CRGB::Blue;
-    leds[2] = CRGB::Blue;
 
-    leds_2[0] = CRGB::Green;
-    leds_2[1] = CRGB::Green;
-    leds_2[2] = CRGB::Green;
+    // EVERY_N_MILLISECONDS(300){
+    //     fadeToBlackBy(leds_2, NUM_LEDS, 20);
+    //     pos++;
+
+    //     leds_2[triOrder[pos]] += CHSV(HUE_GREEN, 255, 192);  
+      
+    //     if (pos == NUM_TRI_LEDS) {
+    //         pos = -1;
+    //     }
+    // }
+    
+    fadeToBlackBy(leds_2, NUM_LEDS, 20);
+    int mapped = beat8(2) % NUM_TRI_LEDS;
+
+    // /int pos = beatsin8( 13, 0, NUM_TRI_LEDS-1 );
+    // Serial.println(pos);
+    leds_2[triOrder[mapped]] += CHSV(gHue, 255, 192);
+
+    // if (!filledIt) {
+    //     leds_2[triOrder[currentTriIndex]] = CRGB::Blue;
+    //     currentTriIndex++;
+    //     if (currentTriIndex > NUM_TRI_LEDS) {
+    //         filledIt = true;
+    //     }
+    //     FastLED.show();
+    //     delay(400);
+    // }
+    
+    // EVERY_N_MILLISECONDS( 5 ) { 
+    //     fadeToBlackBy( leds_2, NUM_LEDS, 10);
+    //  } 
+
+    // if (Serial.available()) {
+    //     String str = Serial.readString();
+    //     currentLed++;
+    //     Serial.print("curent LED: ");
+    //     Serial.println(currentLed);
+    // }
+
+    // leds_2[currentLed] = CRGB::Blue;
+
 
     FastLED.show();
+
+    // insert a delay to keep the framerate modest
+    FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+    EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
---
+++ src/pins.md
@@ -0,0 +1,31 @@
+// 21 - 15
+// 22 - 13
+// 23 - 10
+// 24 - 6
+// 25 - 4
+    
+    // leds_2[26] = CRGB::Red;         // 4
+    // leds_2[27] = CRGB::Blue;        //      12 *
+    // leds_2[28] = CRGB::Green;       //      15 *
+    // leds_2[29] = CRGB::Red;         // 2
+    // leds_2[30] = CRGB::Blue;        // 1
+    // leds_2[31] = CRGB::Green;       // 3
+    // leds_2[32] = CRGB::Red;         // 5
+    // leds_2[33] = CRGB::Blue;        // 9
+    // leds_2[34] = CRGB::Green;       // 8
+
+    // leds_2[35] = CRGB::Red;         // 7
+    // leds_2[36] = CRGB::Blue;        // 12
+    // leds_2[37] = CRGB::Green;       // 14
+
+    // leds_2[38] = CRGB::Red;         // 11
+    // leds_2[39] = CRGB::Blue;        //      8*
+    // leds_2[40] = CRGB::Green;       //      3*
+
+    // leds_2[50] = CRGB::Red;         // 
+    // leds_2[51] = CRGB::Blue;        // 
+    // leds_2[52] = CRGB::Green;       // 
+
+    // leds_2[53] = CRGB::Red;         // 
+    // leds_2[54] = CRGB::Blue;        // 
+    // leds_2[55] = CRGB::Green;       // 
