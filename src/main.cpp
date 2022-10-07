52559f54208969b7688c3fc8b8e0378529589625
Kevin Clement <kevin.clement@gmail.com>
2022-10-06 19:01:04 -0700

    refactor to functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,6 @@
 #include <FastLED.h>
 #include <Bounce2.h>
 
-#define PIN_PI_CONTROL A0
-
-#define LED_1_MUX  4
-#define LED_1_DATA A4
-#define LED_2_MUX  6
-#define LED_2_DATA A5
-
-#define NUM_LEDS 70
-
-#define BRIGHTNESS          96
-#define FRAMES_PER_SECOND  120
-
-// Define the array of leds
-// CRGB leds[NUM_LEDS];
-CRGB leds_2[NUM_LEDS];
-
-// Controls whether to drive LEDs or use external control
-bool inControlOfLEDs = true;
-
 // Raspberry pi IN: A0
 //   A0:  IN: Booted Yes/No 
 // Mux 1:
@@ -30,13 +11,34 @@ bool inControlOfLEDs = true;
 // Mux 2: 
 //   6:  OUT: Mux Control
 //   A5: OUT: LED Data
+#define PIN_PI_CONTROL A0
+#define LED_1_MUX  4
+#define LED_1_DATA A4
+#define LED_2_MUX  6
+#define LED_2_DATA A5
+
+// LED Settings
+#define NUM_LEDS 70
+#define BRIGHTNESS          96
+#define FRAMES_PER_SECOND  120
+#define NUM_TRIANGLE_LEDS 12
+int triangle[] = {25, 29, 30, 31, 32, 33, 38, 37, 21, 22, 23, 24};
+
+// Define the array of leds
+// CRGB leds[NUM_LEDS];
+CRGB leds_2[NUM_LEDS];
+
+// Controls whether to drive LEDs or use external control
+bool inControlOfLEDs = true;
 
 Bounce2::Button button = Bounce2::Button();
+int current_debug_led = 0;
+uint8_t gHue = 0;
 
 void setup() {
     Serial.begin(9600);
     Serial.setTimeout(10);
-    Serial.print("SureShot LED Boot by kevinc...\n");
+    Serial.print("Sure Shot LED Controller by kevinc...\n");
 
     // FastLED.addLeds<WS2812, LED_1_DATA, RGB>(leds, NUM_LEDS);
     FastLED.addLeds<WS2812, LED_2_DATA, GRB>(leds_2, NUM_LEDS)
@@ -52,17 +54,27 @@ void setup() {
     button.setPressedState(LOW); 
 }
 
-int currentLed = 6;
+void animateCircle() {
+    fadeToBlackBy(leds_2, NUM_LEDS, 20);
+    int pos = beat8(2) % NUM_TRIANGLE_LEDS;
+    leds_2[triangle[pos]] += CHSV(gHue, 255, 192);
+}
 
-#define NUM_TRI_LEDS 12
-int triOrder[] = {25, 29, 30, 31, 32, 33, 38, 37, 21, 22, 23, 24};
-
-bool filledIt = false;
-int currentTriIndex = 0;
-uint8_t gHue = 0;
-int pos = -1;
+void animateBackAndForth() {
+    fadeToBlackBy(leds_2, NUM_LEDS, 20);
+    int pos = beatsin16(13, 0, NUM_TRIANGLE_LEDS-1);
+    leds_2[triangle[pos]] += CHSV(gHue, 255, 192);
+}
 
 void loop() {
+    
+    // if (Serial.available()) {
+    //     String str = Serial.readString();
+    //     currentLed++;
+    //     Serial.print("curent LED: ");
+    //     Serial.println(currentLed);
+    // }
+
     button.update();
 
     if ( button.pressed() ) {
@@ -82,53 +94,13 @@ void loop() {
     digitalWrite(LED_1_MUX, inControlOfLEDs ? HIGH : LOW);
     digitalWrite(LED_2_MUX, inControlOfLEDs ? HIGH : LOW);
 
-
-    // EVERY_N_MILLISECONDS(300){
-    //     fadeToBlackBy(leds_2, NUM_LEDS, 20);
-    //     pos++;
-
-    //     leds_2[triOrder[pos]] += CHSV(HUE_GREEN, 255, 192);  
-      
-    //     if (pos == NUM_TRI_LEDS) {
-    //         pos = -1;
-    //     }
-    // }
-    
-    fadeToBlackBy(leds_2, NUM_LEDS, 20);
-    int mapped = beat8(2) % NUM_TRI_LEDS;
-
-    // /int pos = beatsin8( 13, 0, NUM_TRI_LEDS-1 );
-    // Serial.println(pos);
-    leds_2[triOrder[mapped]] += CHSV(gHue, 255, 192);
-
-    // if (!filledIt) {
-    //     leds_2[triOrder[currentTriIndex]] = CRGB::Blue;
-    //     currentTriIndex++;
-    //     if (currentTriIndex > NUM_TRI_LEDS) {
-    //         filledIt = true;
-    //     }
-    //     FastLED.show();
-    //     delay(400);
-    // }
-    
-    // EVERY_N_MILLISECONDS( 5 ) { 
-    //     fadeToBlackBy( leds_2, NUM_LEDS, 10);
-    //  } 
-
-    // if (Serial.available()) {
-    //     String str = Serial.readString();
-    //     currentLed++;
-    //     Serial.print("curent LED: ");
-    //     Serial.println(currentLed);
-    // }
-
-    // leds_2[currentLed] = CRGB::Blue;
-
+    animateCircle();
 
     FastLED.show();
 
     // insert a delay to keep the framerate modest
     FastLED.delay(1000/FRAMES_PER_SECOND); 
 
-    EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+    // slowly cycle the "base color" through the rainbow
+    EVERY_N_MILLISECONDS( 20 ) { gHue++; } 
 }
--- src/pins.md
+++ src/pins.md
@@ -1,31 +1,21 @@
-// 21 - 15
-// 22 - 13
-// 23 - 10
-// 24 - 6
-// 25 - 4
-    
-    // leds_2[26] = CRGB::Red;         // 4
-    // leds_2[27] = CRGB::Blue;        //      12 *
-    // leds_2[28] = CRGB::Green;       //      15 *
-    // leds_2[29] = CRGB::Red;         // 2
-    // leds_2[30] = CRGB::Blue;        // 1
-    // leds_2[31] = CRGB::Green;       // 3
-    // leds_2[32] = CRGB::Red;         // 5
-    // leds_2[33] = CRGB::Blue;        // 9
-    // leds_2[34] = CRGB::Green;       // 8
-
-    // leds_2[35] = CRGB::Red;         // 7
-    // leds_2[36] = CRGB::Blue;        // 12
-    // leds_2[37] = CRGB::Green;       // 14
-
-    // leds_2[38] = CRGB::Red;         // 11
-    // leds_2[39] = CRGB::Blue;        //      8*
-    // leds_2[40] = CRGB::Green;       //      3*
-
-    // leds_2[50] = CRGB::Red;         // 
-    // leds_2[51] = CRGB::Blue;        // 
-    // leds_2[52] = CRGB::Green;       // 
-
-    // leds_2[53] = CRGB::Red;         // 
-    // leds_2[54] = CRGB::Blue;        // 
-    // leds_2[55] = CRGB::Green;       // 
+Playfield: 
+  21: 15
+  22: 13
+  23: 10
+  24: 6
+  25: 4
+  26: 4
+  27: 12 *
+  28: 15 *
+  29: 2
+  30: 1
+  31: 3
+  32: 5
+  33: 9
+  34: 8
+  35: 7
+  36: 12
+  37: 14
+  38: 11
+  39: 8*
+  40: 3*
